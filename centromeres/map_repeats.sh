cbf96758617c077d9323754029e51aed24b7a40b
Harald Grove <haraldgrove@gmail.com>
2026-09-12 16:49:13 +0200

    Update map_repeats.sh

--- centromeres/map_repeats.sh
+++ centromeres/map_repeats.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+target=Sus_scrofa_gca963921485v1.norwegian_landrace.dna.toplevel.fa.gz
+query=sscro_satellites.fasta
+chrom=$1
+output=${chrom}_satellites.paf
+
+singularity exec lastz1.0.4 \
+    lastz \
+    ${target}[subset=${chrom}] \
+    ${query} \
+    --output=${output} \
+    --ambiguous=iupac \
+    --format=PAF
